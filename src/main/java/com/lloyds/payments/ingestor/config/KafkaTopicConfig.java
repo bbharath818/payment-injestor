087b7a409cb0935cd9f2fca4610c15831abcf228
Subrata <subrata.dey88@gmail.com>
2026-05-09 14:35:39 +0530

    3nd commit

---
+++ src/main/java/com/lloyds/payments/ingestor/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.lloyds.payments.ingestor.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+//    @Bean
+//    public NewTopic topic(){
+//        return TopicBuilder.name("payments.submitted")
+//                .partitions(10)
+//                .replicas(4)
+//                .build();
+//    }
+}
