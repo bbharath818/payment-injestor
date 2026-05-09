f7e06a21e9643ea21e542b8ded463d2121447ed9
TEST <TEST@10.138.176.22>
2026-05-09 16:46:34 +0530

    removed commented code

--- src/main/java/com/lloyds/payments/ingestor/config/KafkaTopicConfig.java
+++ src/main/java/com/lloyds/payments/ingestor/config/KafkaTopicConfig.java
@@ -4,11 +4,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaTopicConfig {
-//    @Bean
-//    public NewTopic topic(){
-//        return TopicBuilder.name("payments.submitted")
-//                .partitions(10)
-//                .replicas(4)
-//                .build();
-//    }
 }
