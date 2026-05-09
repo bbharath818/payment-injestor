2801d2175e1a6804f1cc5931f0db47eccf4d0c25
TEST <TEST@10.138.176.22>
2026-05-09 12:34:24 +0530

    Initial commit

---
+++ src/main/java/com/lloyds/payments_ingestor/PaymentsIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.lloyds.payments_ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lloyds/payments_ingestor/PaymentsIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloyds.payments_ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
